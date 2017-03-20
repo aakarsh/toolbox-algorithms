32bcd3ded8e6abe91021dc7e84b6014e5e08632c
Aakarsh Nair <aakarsh.nair@gmail.com>
2017-03-20 23:44:08 +0000

    mod it first to reduce sum

--- week-1/fibonacci_huge/fibonacci_huge.cc
+++ week-1/fibonacci_huge/fibonacci_huge.cc
@@ -1,23 +1,45 @@
 #include <iostream>
+#include <cassert>
+
+long long get_fibonacci_huge_fast(long long n, long long m) {
+  if (n <= 1)
+    return n;
+
+  long long previous = 0;
+  long long current  = 1;
+
+  for (long long i = 0; i < n - 1; ++i) {
+    long long tmp_previous = previous;
+    previous = current;
+    current = (tmp_previous + current) %m;
+  }
+  return current % m;
+}
 
 long long get_fibonacci_huge_naive(long long n, long long m) {
-    if (n <= 1)
-        return n;
+  if (n <= 1)
+    return n;
 
-    long long previous = 0;
-    long long current  = 1;
+  long long previous = 0;
+  long long current  = 1;
 
-    for (long long i = 0; i < n - 1; ++i) {
-        long long tmp_previous = previous;
-        previous = current;
-        current = tmp_previous + current;
-    }
+  for (long long i = 0; i < n - 1; ++i) {
+    long long tmp_previous = previous;
+    previous = current;
+    current = tmp_previous + current;
+  }
 
-    return current % m;
+  return current % m;
 }
 
 int main() {
     long long n, m;
     std::cin >> n >> m;
-    std::cout << get_fibonacci_huge_naive(n, m) << '\n';
+    std::cout << get_fibonacci_huge_fast(n, m) << '\n';
+
+    // for(int i =0 ; i < 10000; i++) {
+    //   std::cerr<<"i="<<i<<std::endl;
+    //   std::cerr<<get_fibonacci_huge_naive(i,10) <<":"<< get_fibonacci_huge_fast(i,10)<<std::endl;
+    //   assert(get_fibonacci_huge_naive(i,10) == get_fibonacci_huge_fast(i,10));
+    //    }
 }

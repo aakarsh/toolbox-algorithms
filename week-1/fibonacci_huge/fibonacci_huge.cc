45c0f5e90c2eb98425684b8964159f3eecc15b07
Aakarsh Nair <aakarsh.nair@gmail.com>
2017-03-21 00:07:00 +0000

    Try endl

--- week-1/fibonacci_huge/fibonacci_huge.cc
+++ week-1/fibonacci_huge/fibonacci_huge.cc
@@ -4,15 +4,15 @@
 long long get_fibonacci_huge_fast(long long n, long long m) {
   if (n <= 1)
     return n;
-
   long long previous = 0;
   long long current  = 1;
-
+  
   for (long long i = 0; i < n - 1; ++i) {
     long long tmp_previous = previous;
     previous = current;
     current = (tmp_previous + current) %m;
   }
+  
   return current % m;
 }
 
@@ -35,6 +35,6 @@ long long get_fibonacci_huge_naive(long long n, long long m) {
 int main() {
   long long n, m;
   std::cin >> n;
-  std::cout << get_fibonacci_huge_fast(n, 10) << '\n';
-
+  std::cout << get_fibonacci_huge_fast(n, 10)<<std::endl;
+  return 0;
 }

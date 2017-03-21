1f23bffc0097f5705129e0f3b8863a9ab802a44c
Aakarsh Nair <aakarsh.nair@gmail.com>
2017-03-21 00:36:58 +0000

    gcd test.

--- week-1/gcd/gcd.cc
+++ week-1/gcd/gcd.cc
@@ -1,4 +1,15 @@
 #include <iostream>
+#include <algorithm>
+
+int gcd_fast(int a, int b) {
+  while(true) {
+    if (a == 0) return b;
+    if (b == 0) return a;
+    if(a > b) std::swap(a,b);
+    b = b%a;
+  }
+  return -1;
+}
 
 int gcd_naive(int a, int b) {
   int current_gcd = 1;
@@ -15,6 +26,6 @@ int gcd_naive(int a, int b) {
 int main() {
   int a, b;
   std::cin >> a >> b;
-  std::cout << gcd_naive(a, b) << std::endl;
+  std::cout << gcd_fast(a, b) << std::endl;
   return 0;
 }

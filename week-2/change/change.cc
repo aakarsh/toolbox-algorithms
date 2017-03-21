0790a2f9f44f358ef0a1ab775dc80f621ebda370
Aakarsh Nair <aakarsh.nair@gmail.com>
2017-03-21 21:11:35 +0000

    Try greedy change.

--- week-2/change/change.cc
+++ week-2/change/change.cc
@@ -1,8 +1,32 @@
 #include <iostream>
+#include <vector>
 
+using std::vector;
+/**
+ * Using greedy stratergy pick coin of least denomination first.  Till
+ * we are below that denomination, then pick next smallest
+ * denomination.
+ */
 int get_change(int m) {
-  //write your code here
-  return m;
+  vector<int> denomination = {10 , 5, 1};
+  vector<int> count(denomination.size(),0);
+  
+  int cur = 0;
+  while(true) {
+    if(m <= 0) break;
+    while(denomination[cur] <= m) {
+      count[cur]++;
+      m = m - denomination[cur];
+    }
+    cur++;
+  }
+  
+  int sum = 0;
+  for(auto s : count) {
+    sum += s;
+  }
+  
+  return sum;
 }
 
 int main() {

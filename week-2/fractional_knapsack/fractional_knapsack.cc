6e41b9fb830718c5d0de34b1d841e7d90464774e
Aakarsh Nair <aakarsh.nair@gmail.com>
2017-03-21 23:13:38 +0000

    fractional greedy knapsack.

--- week-2/fractional_knapsack/fractional_knapsack.cc
+++ week-2/fractional_knapsack/fractional_knapsack.cc
@@ -1,13 +1,58 @@
 #include <iostream>
 #include <vector>
+#include <utility>
+#include <algorithm>
 
 using std::vector;
+using std::pair;
 
+bool pair_by_first(const std::pair<double,int> & p1 , const std::pair<double,int> & p2) {
+  return p1.first > p2.first; 
+}
+/**
+ * Theif will pick the greatest density item till its done. 
+ * moving to next less dense item next
+ */
 double get_optimal_value(int capacity, vector<int> weights, vector<int> values) {
+  
   double value = 0.0;
-
-  // write your code here
-
+  
+  vector<double> fractional(weights.size());
+  vector<pair<double,int>> densities(weights.size());
+  
+  for(int i = 0 ; i < values.size(); i++){
+    densities[i] = std::make_pair((values[i]*1.0) / weights[i], i);
+  }
+  
+  std::sort(densities.begin(), densities.end(),pair_by_first);
+  for(auto d: densities) {
+    //std::cerr<<" "<<d.first<<"["<<d.second<<"]";
+  }
+  
+  //std::cerr<<std::endl;
+  
+  int cur = 0;
+  while(capacity > 0 && cur < densities.size()) {
+    int top = densities[cur].second; // position of most dense item
+    fractional[top] = 1;
+    if( capacity < weights[top]) { // cannot fit most dense item  
+      fractional[top] = capacity/(weights[top]*1.0);
+    }
+    capacity -= fractional[top]*weights[top];
+    cur++;
+  }
+  
+  for(auto dense : densities) {
+    
+    int top = dense.second;
+    double density = dense.first;
+    double qty = fractional[top];
+    
+    //std::cerr<<"qty "<<qty<<" density "<<density<<" pos "<<top<<std::endl;
+    
+    value += (qty*weights[top]) * density;
+    
+  }
   return value;
 }
 

ed24e76a0d90eedf687ee5b85c73ea7e54661f1c
Aakarsh Nair <aakarsh.nair@gmail.com>
2017-03-26 04:10:27 +0000

    Knapsack.

--- week-4/knapsack/knapsack.cc
+++ week-4/knapsack/knapsack.cc
@@ -17,31 +17,45 @@ int optimal_weight(int W, const vector<int> &w) {
   int current_weight = 0;
 
   // Set all weights without any itmes to 0 
-  for(int i = 0; i <= W; i++) {
-    opt_values[0][i] = 0;
-  }
-  
+  for(int i = 0; i <= W; i++) opt_values[0][i] = 0;
   // Set all knapsack with 0-weight to 0 
-  for(int i = 0; i <= nitems; i++) {
-    opt_values[i][0] = 0;
-  }
+  for(int i = 0; i <= nitems; i++) opt_values[i][0] = 0;
 
   for(int i = 1; i <= nitems ; i++) { 
-    for(int j = 1; j <= W; j++) { // for each weight
-      int with_cur = opt_values[i-1][W];
-      int without_cur = opt_values[i-1][W-w[i-1]];
-      if(without_cur+w[i-1] < j) {
-        opt_values[i][j] = std::max(opt_values[i-1][W-w[i-1]]+w[i-1],opt_values[i-1][W]); 
-      } else {
+    
+    int cur_weight = w[i-1]; // 0 indexed
+    
+    for(int j = 1; j <= W; j++) { // for each cur_weight
+      
+      // cur_weight acheived by 0..i-1 with knapsack size j 
+      int with_cur = opt_values[i-1][j];
+      
+      // cur_weight acheived by 0..i-1 with knapsack size of W-w[i-1](cur_weights are zero index)
+      int without_cur = 0;
+      
+      if(j >= cur_weight)
+        without_cur = opt_values[i-1][j-cur_weight];
+      
+      if( without_cur+cur_weight <= j) {
+        if(with_cur <= j) {
+          opt_values[i][j] = std::max(without_cur+cur_weight,with_cur);
+        } else {
+          opt_values[i][j] = without_cur+cur_weight;
+        } 
+      } else if(without_cur <= j) {
         opt_values[i][j] = opt_values[i-1][W];
+      } else {
+        opt_values[i][j] = 0;
       }
+      
     } 
+    
   }
   
   if(debug) {
     for(int i = 0 ; i <= nitems; i++) {
       for(int j = 0; j <= W ; j++) {
-        std::cerr<<std::setw(2)<<opt_values[i][j]<<" ";
+        std::cerr<<std::setw(4)<<opt_values[i][j]<<"("<<i<<","<<j<<")"<<" ";
       }
       std::cerr<<std::endl;
     }
@@ -51,11 +65,14 @@ int optimal_weight(int W, const vector<int> &w) {
 }
 
 int main() {
+  
   int n, W;
   std::cin >> W >> n;
+  
   vector<int> w(n);
   for (int i = 0; i < n; i++) {
     std::cin >> w[i];
   }
+  
   std::cout << optimal_weight(W, w) << '\n';
 }

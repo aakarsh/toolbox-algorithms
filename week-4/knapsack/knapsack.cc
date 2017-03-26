52fd54cf788043217c5b4b97b93a92de0635773b
Aakarsh Nair <aakarsh.nair@gmail.com>
2017-03-26 01:53:01 +0000

    Work through knapsack problem.

--- week-4/knapsack/knapsack.cc
+++ week-4/knapsack/knapsack.cc
@@ -1,17 +1,53 @@
+#include <iomanip>
 #include <iostream>
 #include <vector>
 
 using std::vector;
 
+#ifdef DEBUG
+const bool debug = true;
+#else
+const bool debug = false;
+#endif
+
 int optimal_weight(int W, const vector<int> &w) {
-  //write your code here
+  
+  int nitems = w.size();
+  vector<vector<int>> opt_values(nitems+1, vector<int>(W+1,-1));
   int current_weight = 0;
-  for (size_t i = 0; i < w.size(); ++i) {
-    if (current_weight + w[i] <= W) {
-      current_weight += w[i];
+
+  // Set all weights without any itmes to 0 
+  for(int i = 0; i <= W; i++) {
+    opt_values[0][i] = 0;
+  }
+  
+  // Set all knapsack with 0-weight to 0 
+  for(int i = 0; i <= nitems; i++) {
+    opt_values[i][0] = 0;
+  }
+
+  for(int i = 1; i <= nitems ; i++) { 
+    for(int j = 1; j <= W; j++) { // for each weight
+      int with_cur = opt_values[i-1][W];
+      int without_cur = opt_values[i-1][W-w[i-1]];
+      if(without_cur+w[i-1] < j) {
+        opt_values[i][j] = std::max(opt_values[i-1][W-w[i-1]]+w[i-1],opt_values[i-1][W]); 
+      } else {
+        opt_values[i][j] = opt_values[i-1][W];
+      }
+    } 
+  }
+  
+  if(debug) {
+    for(int i = 0 ; i <= nitems; i++) {
+      for(int j = 0; j <= W ; j++) {
+        std::cerr<<std::setw(2)<<opt_values[i][j]<<" ";
+      }
+      std::cerr<<std::endl;
     }
   }
-  return current_weight;
+  
+  return opt_values[nitems][W];
 }
 
 int main() {
